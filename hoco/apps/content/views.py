3c6d31868569b3718d9c4fe076243d4cc94fad99
Keegan Lanzillotta <swfanboy01@gmail.com>
2019-09-22 19:54:00 -0400

    Update views.py

--- hoco/apps/content/views.py
+++ hoco/apps/content/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from .things import *
+from datetime import datetime
 
 # Create your views here.
 def index(request):
     if "admin" in request.session and request.session["admin"] == True:
-        return render(request, "index.html")
+        now = datetime.now()
+        rally = max(day for day in week if dates[day] < now)
+        return render(
+            request, 
+            "index.html",
+            {
+                "theme": rally_themes[rally],
+                "subtheme": rally_subthemes[rally],
+                "link": rally_links[rally],
+            },
+        )
     else:
         request.session.flush()
         return render(request, "fail.html")
